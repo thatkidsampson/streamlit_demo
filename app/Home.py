808af28804cef3d65031b6b525bea0ee0c4efe43
Peter Canning <petercanning1@gmail.com>
2025-11-11 11:16:02 +0000

    reorganise for clarity

--- app/Home.py
+++ app/Home.py
@@ -66,7 +66,13 @@ if "target_data" in st.session_state:
 
     highlighted_sequence = f"{before_selection}<mark style='background-color: #ffeb3b; padding: 2px; font-weight: bold;'>{selected_portion}</mark>{after_selection}"
     st.markdown(highlighted_sequence, unsafe_allow_html=True)
-    submit_button = st.button("Add new construct boundaries", type="secondary")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        submit_button = st.button("Add construct boundaries", type="secondary")
+    with col2:
+        reset = st.button("Clear stored boundaries", type="primary", on_click=clear_boundaries)
+    with col3:
+        st.page_link("pages/2_Design_primers.py", label="Go to primer design page", icon="🧬")
     if submit_button:
         if selection[0] not in st.session_state.N_term_boundaries:
             st.session_state.N_term_boundaries.append(selection[0])
@@ -80,20 +86,28 @@ if "target_data" in st.session_state:
                 f"Added C-terminal boundary: {sequence_list[selection[1]]}{selection[1] + 1}",
                 icon="✅",
             )
-
-    with st.container(key="Register sequence boundaries"):
-        if st.session_state.N_term_boundaries and st.session_state.C_term_boundaries:
+    st.markdown("#### Current construct info:")
+    with st.container(key="Register sequence boundaries", border=True):
+        # Display useful info about current boundaries and plate capacity
+        col1, col2 = st.columns(2)
+        number_of_constructs = len(st.session_state.N_term_boundaries) * len(st.session_state.C_term_boundaries)
+        progress = number_of_constructs / PLATE_CAPACITY
+        progress_text = f"Current number of constructs: {str(number_of_constructs)} / {PLATE_CAPACITY}"
+        with col1:
             st.info(
                 f"Current N-terminal boundaries: {[x + 1 for x in st.session_state.N_term_boundaries]}"
             )
             st.warning(
                 f"Current C-terminal boundaries: {[x + 1 for x in st.session_state.C_term_boundaries]}"
             )
-        number_of_constructs = len(st.session_state.N_term_boundaries) * len(st.session_state.C_term_boundaries)
-        progress = number_of_constructs / PLATE_CAPACITY
-        my_bar = st.progress(progress, text="Plate capacity used:")
-        progress_text = f"Current number of constructs: {str(number_of_constructs)} / {PLATE_CAPACITY}"
-        st.markdown(progress_text)
+        with col2:
+            if number_of_constructs > PLATE_CAPACITY:
+                st.error(progress_text, icon="🚨")
+                my_bar = st.progress(100, text="Plate over capacity.")
+            else:
+                st.success(progress_text, icon="✅")
+                my_bar = st.progress(progress, text="Plate capacity used:")
+   
         # assign construct names and assemble into a dictionary
         construct_number = 1
         sequence_length = st.session_state.target_data.sequence_length
@@ -141,6 +155,3 @@ if "target_data" in st.session_state:
             theme="streamlit",
             key="my_unique_key",
         )
-
-    reset = st.button("Clear stored boundaries", type="primary", on_click=clear_boundaries)
-    st.page_link("pages/2_Design_primers.py", label="Go to primer design page", icon="🧬")
